10a3866b61c3216d57537fd91d2d4d3b757cc5a9
Travis Royce <traviscroyce@gmail.com>
2023-04-21 22:02:17 -0700

    up

--- pages/2_Player_Size_Tool.py
+++ pages/2_Player_Size_Tool.py
@@ -64,6 +64,15 @@ color: #ffffff;
 """
 st.markdown(custom_metric_color, unsafe_allow_html=True)
 
+custom_columns = """
+<style>
+[data-testid="column"] {
+background: linear-gradient(to right, #2c3333, #1c2024);
+}
+</style>
+"""
+st.markdown(custom_columns, unsafe_allow_html=True)
+
 # get current time in pst
 pst = datetime.timezone(datetime.timedelta(hours=-8))
 # to datetime
