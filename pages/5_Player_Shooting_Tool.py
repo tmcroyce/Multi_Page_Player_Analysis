e0aa7a1b7eab965ff03e845e0e8ad5c58076eaed
Travis Royce <traviscroyce@gmail.com>
2023-04-21 19:55:42 -0700

    u

--- pages/5_Player_Shooting_Tool.py
+++ pages/5_Player_Shooting_Tool.py
@@ -52,15 +52,15 @@ st.markdown(custom_css, unsafe_allow_html=True)
 # Inject the custom CSS into the Streamlit app
 st.markdown(custom_css2, unsafe_allow_html=True)
 
-custom_css3 = """
-<style>
-[section] {
-background:linear-gradient(to right, #F5B663, #9E60F0);
-}
-</style>
-"""
+# custom_css3 = """
+# <style>
+# [section] {
+# background:linear-gradient(to right, #F5B663, #9E60F0);
+# }
+# </style>
+# """
 
-st.markdown(custom_css3, unsafe_allow_html=True)
+# st.markdown(custom_css3, unsafe_allow_html=True)
 
 custom_css33 = """
 <style>
